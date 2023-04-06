a9839a9be02b32285f6ab6f53ab9bfce635e6ddf
Yourui <yourui.ruan@gmail.com>
2023-04-06 13:12:17 -0400

    Merged client and server package.json locks
    Moved package.json and related folders to root directory
    
    Added concurrently package to start both
    server and vite with 'npm start'

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path')
+
 const app = express();
 
 app.get('/api', (req, res) => {
   res.json({"users": ["user1", "user2", "user3"]})
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+})
 
 app.listen(4000, () => {console.log('server started on port 4000')});
