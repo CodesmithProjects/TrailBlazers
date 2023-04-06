6466cd4b4a1686754f71d3934fd95c8aa0d75133
Yourui <yourui.ruan@gmail.com>
2023-04-05 22:18:24 -0400

    boilerplate vite express completed

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState([{}]);
+  useEffect(() => {
+    fetch('/api').then(
+      response => response.json()
+    ).then(
+      data => {
+        setData(data);
+      }
+    )
+  }, []);
+  
+  return(
+    <div>
+      {(typeof data.users === 'undefined') ?
+        (<p>Loading</p>) :
+        ( data.users.map ((user, i) => (
+          <p key={i}>{user}</p>
+        )))
+      }
+    </div>
+  )
+}
+
+
+export default App
