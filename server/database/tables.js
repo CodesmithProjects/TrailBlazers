149c2f21799ac93ab2fa46e3ae2e39d372bd1f25
Jenny Schmalz <jschmalzperez@gmail.com>
2023-04-12 13:36:01 -0700

    update database tables/querys

--- server/database/tables.js
+++ server/database/tables.js
@@ -12,5 +12,13 @@ CREATE TABLE accounts (
 CREATE TABLE favorite_trails (
   fav_id serial PRIMARY KEY,
   user_id integer REFERENCES accounts (user_id),
-  trail_api VARCHAR ( 255 ) UNIQUE NOT NULL
+  trail_api VARCHAR ( 255 ) NOT NULL,
+  trail_name VARCHAR ( 255 ) NOT NULL
 );
+
+// inner join query
+
+SELECT accounts.user_id, trail_api, trail_name
+FROM accounts
+INNER JOIN favorite_trails
+ON favorite_trails.user_id = accounts.user_id;
