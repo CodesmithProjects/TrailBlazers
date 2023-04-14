ef5791716d1ba8ae4c1048be548f899a0fa47ae6
EstherNahm <efnahm@gmail.com>
2023-04-14 16:48:22 -0600

    Revert "Merge pull request #28 from CodesmithProjects/finish-sessions"
    This reverts commit cb8acba43cf32df3467931553602b37871d49635, reversing
    changes made to f69dbeaadda7ec2ad97c77df32eda189b6cf4ef8.

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -1,41 +1,7 @@
-const db = require('../models/bikeTrailsModels');
 const sessionController = {};
 
-sessionController.createSession = async (req, res, next) => {
-    try {
-        const findQueryString = `SELECT * FROM sessions WHERE google_id = '${res.locals.userID}' AND created_at >= (now() - interval '15 minutes')`;
-        const tryToFindSession = await db.query(findQueryString);
-        if (!tryToFindSession.rows[0]) {
-            const createQueryString = `INSERT INTO sessions (google_id) VALUES ('${res.locals.userID}')`;
-            const createRes = await db.query(createQueryString)
-        }
-        return next();
-    } catch {
-        return next({log:'problem at createSession middleware', message: 'problem creating session in database'});
-    }
-}
-
-sessionController.checkSession = async (req, res, next) => {
-    try {
-        const userID = req.cookies.userID;
-        const findQueryString = `SELECT * FROM sessions WHERE google_id = '${userID}' AND created_at >= (now() - interval '15 minutes')`;
-        const tryToFindSession = await db.query(findQueryString);
-        if (!tryToFindSession.rows[0]) {
-            return res.redirect('http://localhost:5173');
-        }
-        return next();
-    } catch {
-        return next({log:'problem at checkSession middleware', message: 'problem finding session in database'})
-    }
-}
-
-sessionController.deleteOldSessions = async (req, res, next) => {
-    try {
-        const deleteString = `DELETE FROM sessions WHERE created_at < (now() - interval '15 minutes')`;
-        await db.query(deleteString);
-    } catch {
-        return next({log:'problem at deletedOldSessions middleware', message: 'problem deleting old sessions in database'})
-    }
+sessionController.createSession = (req, res, next) => {
+    
 }
 
 module.exports = sessionController;
