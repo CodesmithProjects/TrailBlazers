402dd44e66aec64a7eb04897c32a63c132889acc
Kareem Saleh <kareemhs@gmail.com>
2023-04-20 22:35:03 -0400

    setup environment variables

--- client/src/TrailDetailsWeatherCard.jsx
+++ client/src/TrailDetailsWeatherCard.jsx
@@ -1,34 +1,34 @@
-import React from "react";
-import ReactWeather, { useOpenWeather } from "react-open-weather";
+import React from 'react';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
 
 export default function TrailDetailsWeatherCard({ trail }) {
   // sourced from https://www.npmjs.com/package/react-open-weather
   const { data, isLoading, errorMessage } = useOpenWeather({
-    key: `${import.meta.env.OPENWEATHERAPIKEY}`,
+    key: `${import.meta.env.OPEN_WEATHER_API_KEY}`,
     lat: `${trail.lat}`,
     lon: `${trail.lon}`,
-    lang: "en",
-    unit: "imperial", // values are (metric, standard, imperial)
+    lang: 'en',
+    unit: 'imperial', // values are (metric, standard, imperial)
   });
 
   const customStyles = {
-    fontFamily: "Roboto, sans-serif",
-    gradientStart: "rgba(255, 255, 255, 0.05)",
-    gradientMid: "rgba(255, 255, 255, 0.05)",
-    gradientEnd: "rgba(255, 255, 255, 0.05)",
-    locationFontColor: "#FFF",
-    todayTempFontColor: "#FFF",
-    todayDateFontColor: "#B5DEF4",
-    todayRangeFontColor: "#B5DEF4",
-    todayDescFontColor: "#B5DEF4",
-    todayInfoFontColor: "#B5DEF4",
-    todayIconColor: "#B5DEF4",
-    forecastBackgroundColor: "rgba(255, 255, 255, 0.05)",
-    forecastSeparatorColor: "#DDD",
-    forecastDateColor: "#FFF",
-    forecastDescColor: "#FFF",
-    forecastRangeColor: "#FFF",
-    forecastIconColor: "#B5DEF4",
+    fontFamily: 'Roboto, sans-serif',
+    gradientStart: 'rgba(255, 255, 255, 0.05)',
+    gradientMid: 'rgba(255, 255, 255, 0.05)',
+    gradientEnd: 'rgba(255, 255, 255, 0.05)',
+    locationFontColor: '#FFF',
+    todayTempFontColor: '#FFF',
+    todayDateFontColor: '#B5DEF4',
+    todayRangeFontColor: '#B5DEF4',
+    todayDescFontColor: '#B5DEF4',
+    todayInfoFontColor: '#B5DEF4',
+    todayIconColor: '#B5DEF4',
+    forecastBackgroundColor: 'rgba(255, 255, 255, 0.05)',
+    forecastSeparatorColor: '#DDD',
+    forecastDateColor: '#FFF',
+    forecastDescColor: '#FFF',
+    forecastRangeColor: '#FFF',
+    forecastIconColor: '#B5DEF4',
   };
 
   return (
@@ -40,7 +40,7 @@ export default function TrailDetailsWeatherCard({ trail }) {
         data={data}
         lang="en"
         locationLabel={trail.city}
-        unitsLabels={{ temperature: "F", windSpeed: "Km/h" }}
+        unitsLabels={{ temperature: 'F', windSpeed: 'Km/h' }}
         showForecast
       />
     </div>
