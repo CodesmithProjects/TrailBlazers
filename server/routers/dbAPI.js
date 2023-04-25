f0f2cc2c1a87f39544b39c390ca0d1e3ca8bc2a6
scottdeutsch40 <scottdeutsch40@gmail.com>
2023-04-24 22:28:05 -0400

    Edit Review feature working

--- server/routers/dbAPI.js
+++ server/routers/dbAPI.js
@@ -21,8 +21,16 @@ dbRouter.delete('/deleteFavoriteTrail/:trailId', sessionController.authenticateU
 dbRouter.post('/createReview/:trailID', sessionController.authenticateUser, reviewController.createReview, (req, res) => {
   return res.status(201).json('created review')
 })
+
 dbRouter.delete('/deleteReview/:trail_reviewID', sessionController.authenticateUser, reviewController.deleteReview, (req, res) => {
   return res.status(201).json('deleted review')
 })
 
+dbRouter.get('/getReview/:trail_reviewID', sessionController.authenticateUser, reviewController.getReview, (req, res) => {
+  return res.status(201).json((res.locals.review_data))
+})
+
+dbRouter.patch('/updateReview/:trail_reviewID', sessionController.authenticateUser, reviewController.updateReview, (req, res) => {
+  return res.status(201).json('updated review')
+})
 module.exports = dbRouter;
