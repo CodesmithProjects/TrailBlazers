d4448c670cdec753816c7c2b74bec707cbd94add
larkinaj <larkin.aj@gmail.com>
2023-04-27 16:11:32 -0400

    Modified width of modal window in UserPhotos.jsx

--- client/src/UserPhotos.jsx
+++ client/src/UserPhotos.jsx
@@ -214,7 +214,7 @@ export default function UserPhotos({trail}) {
               top: "50%",
               left: "50%",
               transform: "translate(-50%, -50%)",
-              width: 950,
+              width: 1050,
               bgcolor: "background.paper",
               border: "2px solid #000",
               boxShadow: 24,
